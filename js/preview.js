c136ef7d832bf70d1c788612fc40fc8333027ec2
VadeemLeonov <leonov.va@bk.ru>
2019-07-26 22:23:46 +0300

    Добавил сортировку фото о фильтрам

--- js/preview.js
+++ js/preview.js
@@ -3,51 +3,57 @@
 (function () {
 
   // Показываем большое фото
-  var userPicture = document.querySelector('.big-picture');
-  var body = document.querySelector('body');
 
-  var getBigPicture = function (item) {
-    userPicture.classList.remove('hidden');
-    body.classList.add('.modal-open');
-    document.addEventListener('keydown', onBigPictureEscPress);
-    userPicture.querySelector('img').src = item.url;
-    userPicture.querySelector('.likes-count').textContent = item.like;
-    userPicture.querySelector('.comments-count').textContent = item.comments.length;
-    userPicture.querySelector('.social__caption').textContent = item.description;
-    window.comment.createElement(item.comments);
-  };
-
-  var addPictureClickHandler = function (pictureItem, dataCard) {
-    pictureItem.addEventListener('click', function () {
-      getBigPicture(dataCard);
-    });
-  };
-
-  var onLoad = function (response) {
-    var pictureItems = document.querySelectorAll('.picture');
-    for (var i = 0; i < pictureItems.length; i++) {
-      addPictureClickHandler(pictureItems[i], response[i]);
+  var openFoto = {
+    userPicture: document.querySelector('.big-picture'),
+    getBigPicture: function (item) {
+      window.comment.delCommentElement();
+      openFoto.userPicture.classList.remove('hidden');
+      document.querySelector('body').classList.add('.modal-open');
+      document.addEventListener('keydown', closeFoto.onBigPictureEscPress);
+      openFoto.userPicture.querySelector('img').src = item.url;
+      openFoto.userPicture.querySelector('.likes-count').textContent = item.like;
+      openFoto.userPicture.querySelector('.comments-count').textContent = item.comments.length;
+      openFoto.userPicture.querySelector('.social__caption').textContent = item.description;
+      window.comment.createElement(item.comments);
+    },
+    addPictureClickHandler: function (pictureItem, dataCard) {
+      pictureItem.addEventListener('click', function () {
+        openFoto.getBigPicture(dataCard);
+      });
+    },
+    onLoad: function (response) {
+      var pictureItems = document.querySelectorAll('.picture');
+      for (var i = 0; i < pictureItems.length; i++) {
+        openFoto.addPictureClickHandler(pictureItems[i], response[i]);
+      }
     }
   };
 
-  window.backend.load(onLoad, window.util.onError);
+  window.backend.load(openFoto.onLoad, window.util.onError);
+
   // Закрываем большое фото
 
-  var bigPictureCansel = document.querySelector('#picture-cancel');
-
-  var onBigPictureEscPress = function (evt) {
-    if (window.util.isEscEvent(evt)) {
-      closeBigPicture();
+  var closeFoto = {
+    bigPictureCansel: document.querySelector('#picture-cancel'),
+    onBigPictureEscPress: function (evt) {
+      if (window.util.isEscEvent(evt)) {
+        closeFoto.closeBigPicture();
+      }
+    },
+    closeBigPicture: function () {
+      window.comment.delCommentElement();
+      openFoto.userPicture.classList.add('hidden');
+      openFoto.userPicture.classList.remove('modal-open');
+      document.removeEventListener('keydown', closeFoto.onBigPictureEscPress);
     }
   };
 
-  var closeBigPicture = function () {
-    userPicture.classList.add('hidden');
-    userPicture.classList.remove('modal-open');
-    document.removeEventListener('keydown', onBigPictureEscPress);
-  };
-
-  bigPictureCansel.addEventListener('click', function () {
-    closeBigPicture();
+  closeFoto.bigPictureCansel.addEventListener('click', function () {
+    closeFoto.closeBigPicture();
   });
+
+  window.preview = {
+    reload: openFoto.onLoad
+  };
 })();
